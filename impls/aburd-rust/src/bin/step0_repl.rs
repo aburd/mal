0c484778a683d0e89f5da90b1f6fb3a445e74def
Aaron Burdick <aaron.burdick@protonmail.com>
2024-05-11 21:38:01 +0900

    refactor: add editing line support

--- impls/aburd-rust/src/bin/step0_repl.rs
+++ impls/aburd-rust/src/bin/step0_repl.rs
@@ -1,22 +1,37 @@
-use std::io::{self, Stdin, Stdout, Write};
+use rustyline::error::ReadlineError;
+use rustyline::{DefaultEditor, Result};
 
-fn rep(stdout: &mut Stdout, stdin: &Stdin) -> io::Result<()> {
-    print!("user> ");
-    stdout.flush()?;
-
-    let mut buffer = String::new();
-    stdin.read_line(&mut buffer)?;
-
-    print!("{}", buffer);
-
-    Ok(())
-}
-
-fn main() {
-    let mut stdout = io::stdout();
-    let stdin = io::stdin();
+fn main() -> Result<()> {
+    // `()` can be used when no completer is required
+    let mut rl = DefaultEditor::new()?;
+    #[cfg(feature = "with-file-history")]
+    if rl.load_history("history.txt").is_err() {
+        println!("No previous history.");
+    }
 
     loop {
-        rep(&mut stdout, &stdin).unwrap();
+        let readline = rl.readline("user> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
+                println!("{}", line);
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
     }
+
+    #[cfg(feature = "with-file-history")]
+    rl.save_history("history.txt");
+    Ok(())
 }
